e512be89e7f8e978acefc8995d0da91e09231035
Anthony Huynh <anthonyhuynh1204@gmail.com>
2024-12-03 14:16:17 +0100

    feat: added auth routing

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/pirsma";
+import { compare } from "bcryptjs";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "email@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null;
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (user && (await compare(credentials.password, user.password))) {
+          return { id: user.id, email: user.email };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/pirsma";
+import { userSchema } from "@/utils/validationSchemas";
+import { hash } from "bcryptjs";
+import { ZodError } from "zod";
+
+export async function POST(request: NextRequest) {
+  try {
+    const data = await request.json();
+    const parsedData = userSchema.parse(data);
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email: parsedData.email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Email already in use" },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await hash(parsedData.password, 10);
+
+    await prisma.user.create({
+      data: {
+        email: parsedData.email,
+        password: hashedPassword,
+        phoneNotification: parsedData.phoneNotification,
+        phoneNumber: parsedData.phoneNumber,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json({ errors: error.errors }, { status: 400 });
+    } else if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    } else {
+      return NextResponse.json(
+        { error: "An unexpected error occurred" },
+        { status: 500 }
+      );
+    }
+  }
+}
